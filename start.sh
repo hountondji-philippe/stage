66a29eccce9a63254a144ebcc0e97c426593314a
Philippe Hountondji <hountondjiphilippe58@gmail.com>
2026-07-29 11:22:54 +0100

    email

--- start.sh
+++ start.sh
@@ -3,5 +3,13 @@ php artisan config:clear
 php artisan config:cache
 php artisan migrate --force
 php artisan db:seed --force
-php artisan queue:work --tries=3 --timeout=90 &
+
+(
+  while true; do
+    php artisan queue:work --tries=3 --timeout=90 --sleep=3
+    echo "queue:work s'est arrêté, redémarrage dans 2 secondes..."
+    sleep 2
+  done
+) &
+
 php artisan serve --host 0.0.0.0 --port ${PORT:-10000}
